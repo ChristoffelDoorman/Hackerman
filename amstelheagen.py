797b4f141444961d488b89fd6c964b7a98b17b25
Stof95 <31651301+Stof95@users.noreply.github.com>
2017-11-07 14:36:08 +0100

    Add files via upload

--- amstelheagen.py
+++ amstelheagen.py
@@ -7,23 +7,27 @@ import matplotlib.patches as patches
 import helpers
 
 
-def floorMap(building, x, y):
-	fig1 = plt.figure()
-	ax1 = fig1.add_subplot(111, aspect='equal')
+def drawBuilding(building, x, y):
+
+	# add building to map
 	ax1.add_patch(
 	    patches.Rectangle(
 	        (x, y),   			# (x,y)
-	        building.length,    # width
-	        building.width,     # height
+	        building.length,    # length
+	        building.width,     # width
 	    )
 	)
+
+if __name__ == "__main__":
+	fig1 = plt.figure()
+	ax1 = fig1.add_subplot(111, aspect='equal')
+	
+	floorMap(helpers.bungalow, 50, 20)
+	floorMap(helpers.maison, 75, 30)
+	floorMap(helpers.house, 30, 70)
+
 	ax1.set_xlim(0,180)
 	ax1.set_ylim(0,160)
 	fig1.savefig('rect1.png', dpi=90, bbox_inches='tight')
 
-
-if __name__ == "__main__":
-	floorMap(helpers.bungalow, 50, 20)
-	floorMap(helpers.maison, 75, 30)
-	floorMap(helpers.house, 30, 70)
 
