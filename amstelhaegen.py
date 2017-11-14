348cd269c6b74a91d9cad9bc7c38513130e7c486
jansentim1 <twj.jansen@gmail.com>
2017-11-14 12:33:06 +0100

    update

--- amstelhaegen.py
+++ amstelhaegen.py
@@ -35,22 +35,32 @@ def overlap(building1, building2):
 if __name__ == "__main__":
 	fig1 = plt.figure()
 	ax1 = fig1.add_subplot(111, aspect='equal')
+	# fill building array with a house at a random point
+	buildings = [classes.house(0,0)]
 
-	buildings = []
-
-	for i in range(12):
+	while len(buildings) <= 12:
+		print len(buildings)
 		xrandom = random.randint(0, 170)
 		yrandom = random.randint(0, 150)
-
-		buildings.append(house(xrandom, yrandom))
-
 		for building in buildings:
-			overlap(buildings[building],  )
+			print building
+			print overlap(building, classes.house(xrandom, yrandom))
+			if overlap(building, classes.house(xrandom, yrandom)):
+				break
+		buildings.append(classes.house(xrandom, yrandom))
+		drawBuilding(classes.house, xrandom, yrandom, "red")
 
-			while (overlap(building, building[building]) = True)
-			xrandom = random.randint(0, 170)
-			yrandom = random.randint(0, 150)
-			drawBuilding(classes.house, xrandom, yrandom, "red")
+	#for i in buildings
+	#print overlap(buildings[0], buildings[1])
+
+		#for i in buildings:
+			#overlap(buildings[building],  )
+
+			# while (overlap(building, building[building]) = True)
+			#xrandom = random.randint(0, 170)
+			#yrandom = random.randint(0, 150)
+			#drawBuilding(classes.house, xrandom, yrandom, "red")
+	# print buildings
 
 	for i in range(5):
 		xrandom = random.randint(0, 170)
--- classes.py
+++ classes.py
@@ -2,18 +2,19 @@
 
 
 class house:
-
 	length = 12
 	width = 12
-	price = 285.000
-	marginalValue = 1.03
 
 	def __init__(self, x, y):
+		self.length = 12
+		self.width = 12
+		self.price = 285.000
+		self.marginalValue = 1.03
 		self.location = (x, y)
 		self.left = x
-		self.right = x + width
+		self.right = x + self.width
 		self.bottom = y
-		self.top = y + length
+		self.top = y + self.length
 
 	def score(self, distanceNeighbour):
 		self.freeSpace = distanceNeighbour
