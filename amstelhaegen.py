4fabd54a8efa4e5ec4ab9f261beea1c0458b139a
jansentim1 <twj.jansen@gmail.com>
2017-11-21 13:37:15 +0100

    update

--- amstelhaegen.py
+++ amstelhaegen.py
@@ -29,39 +29,30 @@ def overlap(building1, building2):
 		or building1.bottom > building2.top or building2.bottom > building1.top):
 		overlap = False
 
-	if not overlap:
-		return False
-
-	return True
+	return overlap
 
 if __name__ == "__main__":
 	fig1 = plt.figure()
 	ax1 = fig1.add_subplot(111, aspect='equal')
 	ax1.set_xlim(0,360)
 	ax1.set_ylim(0,320)
+
 	# fill building array with a house at a random point
 	buildings = []
+	xrandom = random.randint(0, 336)
+	yrandom = random.randint(0, 296)
+	buildings.append(classes.house(xrandom, yrandom))
 
 	while len(buildings) < 60:
 		xrandom = random.randint(0, 336)
 		yrandom = random.randint(0, 296)
 		house = classes.house(xrandom, yrandom)
 
-		if len(buildings) == 0:
-			buildings.append(house)
-			left = random.randint(0, 336)
-			bottom = random.randint(0, 296)
-			house = classes.house(left, bottom)
-
 		olap = True
 		for building in buildings:
 			olap = overlap(house, building)
 
 			if olap:
-				house.left = random.randint(0, 336)
-				house.bottom = random.randint(0, 296)
-				house.right = house.left + 24
-				house.top = house.bottom + 24
 				break
 
 		if not olap:
