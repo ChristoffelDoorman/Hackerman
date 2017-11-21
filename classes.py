e31a35c6c252dbe017b55d2b554ebe2207e6f758
jansentim1 <twj.jansen@gmail.com>
2017-11-21 13:42:56 +0100

    update

--- classes.py
+++ classes.py
@@ -19,30 +19,35 @@ class house:
 
 
 class bungalow:
-
-	length = 10.5
-	width = 13
-	price = 399.000
-	marginalValue = 1.04
+	# length = 10.5
+	# width = 13
+	# price = 399.000
+	# marginalValue = 1.04
 
 	def __init__(self, x, y):
-		self.location = (x, y)
-
-	def score(self, distanceNeighbour):
-		self.freeSpace = distanceNeighbour
-		value = price * marginalValue * self.freespace
-
+		self.left = x
+		self.right = x + 21
+		self.bottom = y
+		self.top = y + 26
+    #
+	# def score(self, distanceNeighbour):
+	# 	self.freeSpace = distanceNeighbour
+	# 	value = price * marginalValue * self.freespace
+    #
 
 class maison:
-
-	length = 16.5
-	width = 17
-	price = 610.000
-	marginalValue = 1.06
+    #
+	# length = 16.5
+	# width = 17
+	# price = 610.000
+	# marginalValue = 1.06
 
 	def __init__(self, x, y):
-		self.location = (x, y)
-
-	def value(self, distanceNeighbour):
-		self.freeSpace = distanceNeighbour
-		value = price * marginalValue * self.freespace
+		self.left = x
+		self.right = x + 33
+		self.bottom = y
+		self.top = y + 34
+    #
+	# def value(self, distanceNeighbour):
+	# 	self.freeSpace = distanceNeighbour
+	# 	value = price * marginalValue * self.freespace
